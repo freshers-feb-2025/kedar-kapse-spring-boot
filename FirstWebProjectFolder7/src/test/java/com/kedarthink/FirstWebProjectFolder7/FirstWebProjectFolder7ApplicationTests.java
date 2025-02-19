a82ed9ac6436defe5fb9fc657614df743bbb658f
Kedar Kapse <kedar.kapse@thinkitive.com>
2025-02-19 17:50:22 +0530

    SpringBoot Web.

---
+++ FirstWebProjectFolder7/src/test/java/com/kedarthink/FirstWebProjectFolder7/FirstWebProjectFolder7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kedarthink.FirstWebProjectFolder7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstWebProjectFolder7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
