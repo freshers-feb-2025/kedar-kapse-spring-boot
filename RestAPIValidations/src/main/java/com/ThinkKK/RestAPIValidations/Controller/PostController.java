ee36ccb46b15208f45840a56eb70d92e32d233bf
Kedar Kapse <kedar.kapse@thinkitive.com>
2025-02-20 19:27:51 +0530

    Response Status , Valedating API , Customise APIS , Actuator Basics

--- RestAPIValidations/src/main/java/com/ThinkKK/RestAPIValidations/Controller/PostController.java
+++ RestAPIValidations/src/main/java/com/ThinkKK/RestAPIValidations/Controller/PostController.java
@@ -1,4 +1,60 @@
 package com.ThinkKK.RestAPIValidations.Controller;
 
+import com.ThinkKK.RestAPIValidations.Entity.Post;
+import com.ThinkKK.RestAPIValidations.Entity.User;
+import com.ThinkKK.RestAPIValidations.Reposotory.PostRepository;
+import com.ThinkKK.RestAPIValidations.Service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
 public class PostController {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public PostController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @PostMapping("/addpost")
+    public ResponseEntity<Post> addPost(@Valid @RequestBody Post post) {
+        Post creratedPost = postRepository.save(post);
+        return ResponseEntity.created(URI.create("/user/"+creratedPost.getId())).body(creratedPost);
+    }
+
+
+
+    @GetMapping("/allPost")
+    public ResponseEntity<List<Post>> getAllUsers() {
+        List<Post> posts = postRepository.findAll();
+        if(posts.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok().body(posts);
+    }
+
+    @GetMapping("/{id}")
+    public Post getPostById(@PathVariable Long id) {
+        return postRepository.findById(id).orElseThrow(()->new RuntimeException("Value Not Found :- "+id));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deletePosts(@PathVariable Long id) {
+        postRepository.deleteById(id);
+    }
+
+        @PutMapping("/updatepost/{id}")
+        public Post updatePost(@PathVariable Long id , @RequestBody Post post){
+                return postRepository.findById(id).map(existingUser ->{
+                    existingUser.setId(post.getId());
+                    existingUser.setDescription(post.getDescription());
+                    return postRepository.save(existingUser);
+                }).orElseThrow(null);
+        }
 }
--- RestAPIValidations/src/main/java/com/ThinkKK/RestAPIValidations/Service/UserService.java
+++ RestAPIValidations/src/main/java/com/ThinkKK/RestAPIValidations/Service/UserService.java
@@ -1,4 +1,52 @@
 package com.ThinkKK.RestAPIValidations.Service;
 
+import com.ThinkKK.RestAPIValidations.Entity.User;
+import com.ThinkKK.RestAPIValidations.Reposotory.UserRepository;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import java.util.List;
+
+
+
+@Service
 public class UserService {
+
+   // private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    @Autowired
+    private UserRepository userRepository;
+    public UserService(UserRepository userRepository){
+        this.userRepository=userRepository;
+    }
+
+    public User saveData(User user){
+        User savedUser = userRepository.save(user);
+        //logger.info("User saved: {}", savedUser);
+        return savedUser;
+    }
+
+    public List<User> findAll(){
+        List<User> allUsers = userRepository.findAll();
+       // logger.info("Retrieved {} users : ",allUsers.size());
+        return allUsers;
+    }
+
+    public User getOne(Long id){
+        return userRepository.findById(id).orElseThrow(()->new RuntimeException("id not present "+id));
+    }
+
+    public void deleteById(Long id){
+        userRepository.deleteById(id);
+    }
+
+    public User updateUser(Long id , User updatedUser){
+        return userRepository.findById(id).map(existingUser ->{
+            existingUser.setName(updatedUser.getName());
+            existingUser.setBirthDate(updatedUser.getBirthDate());
+            return userRepository.save(existingUser);
+        }).orElseThrow(null);
+    }
+
 }
