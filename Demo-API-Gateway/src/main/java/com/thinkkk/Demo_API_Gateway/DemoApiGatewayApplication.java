1bc07135e8228c6a3ea2f4f7e247edefebb09682
Kedar Kapse <kedar.kapse@thinkitive.com>
2025-02-24 19:58:31 +0530

    Created a simple project using microservices architecture, including the following services: Demo Admin Server, Demo Service Registry, Demo API Gateway, Demo Greet API, Demo Welcome API

---
+++ Demo-API-Gateway/src/main/java/com/thinkkk/Demo_API_Gateway/DemoApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.thinkkk.Demo_API_Gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class DemoApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApiGatewayApplication.class, args);
+	}
+
+}
---
+++ Demo-Welcome-API/src/main/java/com/thinkkk/Demo_Welcome_API/DemoWelcomeApiApplication.java
@@ -0,0 +1,13 @@
+package com.thinkkk.Demo_Welcome_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWelcomeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWelcomeApiApplication.class, args);
+	}
+
+}
